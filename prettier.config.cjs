25d04586d645d6451b06f1e7a324e56a4aeeb4b5
Thien Nguyen <nvktvan@gmail.com>
2023-06-03 22:23:08 -0700

    Set Up II

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,11 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-};
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+}
 
-module.exports = config;
+module.exports = config
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export default authMiddleware();
 
-console.log("middleware.ts is running?");
+console.log("middleware.ts");
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
